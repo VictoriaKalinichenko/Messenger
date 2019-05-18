50f5aaca1f06d3301e0ae83b6b7b1e940ab9923b
Max Novikov <maksym.novikov@outlook.com>
2019-05-18 21:42:01 +0300

    Add project files.

---
+++ Messenger.BusinessLogic/Hubs/MessengerHub.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System.Threading.Tasks;
+
+namespace Messenger.BusinessLogic.Hubs
+{
+    public class MessengerHub : Hub
+    {
+        public async Task SendMessage(string userId, string message)
+        {
+            await Clients.All.SendAsync("ReceiveMessage", userId, message);
+        }
+    }
+}
