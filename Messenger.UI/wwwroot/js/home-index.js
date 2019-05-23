38fe86c5338a49c548eb4c4bec0415c8375f3356
victoria.kalinichenko <victoria.kalinichenko@outlook.com>
2019-05-23 22:30:31 +0300

    Added view of user list

--- Messenger.UI/wwwroot/js/home-index.js
+++ Messenger.UI/wwwroot/js/home-index.js
@@ -7,10 +7,24 @@ function getUserList() {
     $.ajax({
         url: '/api/User/GetList',
         success: function (data) {
-            console.log(data);
+            showUserList(data);
         },
-        error: function (error) {
-            console.log(error);
+        error: function () {
+            swal("Error", "Some error was occurred while processing your request!");
         }
+    });
+}
+
+function showUserList(userList) {
+
+    userList.forEach(function (item) {
+        var li = document.createElement("li");
+        li.id = item.id;
+        document.getElementById("userList").appendChild(li);
+
+        var link = document.createElement("a");
+        link.href = "#";
+        link.textContent = item.userName;
+        document.getElementById(item.id).appendChild(link);
     });
 }
